c8f92bca857e0bb35eaf5228388258967681dc83
Akhileshait <akhileshpatidar989368@gmail.com>
2024-02-06 18:27:22 +0530

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
