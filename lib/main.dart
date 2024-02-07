e654e1bd0b6d83ffe658e62850ce2266922a55bb
Akhileshait <akhileshpatidar989368@gmail.com>
2024-02-07 22:24:04 +0530

    trasaction page

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,12 @@ import 'package:fl_chart/fl_chart.dart';
 import 'dart:math';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
 }
+
+const List<Widget> fruits = <Widget>[Text('INCOME'), Text('EXPENSE')];
 
 class MyApp extends StatefulWidget {
   MyApp({super.key});
@@ -55,141 +59,195 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: SafeArea(
-            child: Column(
-          children: [
-            AppBar(
-              title: const Text('FINTRACK'),
-              centerTitle: true,
-              backgroundColor: Colors.grey.shade400,
+    return Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        children: [
+          AppBar(
+            title: const Text('FINTRACK'),
+            centerTitle: true,
+            backgroundColor: Colors.grey.shade400,
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10),
             ),
-            SizedBox(
-              height: 10,
+            child: DropdownButton<String>(
+              hint: Text("Choose"),
+              focusColor: Colors.amberAccent,
+              items: <String>['Daily', 'Weekly', 'Monthly', 'Annually']
+                  .map((String value) {
+                return DropdownMenuItem<String>(
+                  value: value,
+                  child: Text(value),
+                );
+              }).toList(),
+              onChanged: (_) {},
             ),
-            Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: DropdownButton<String>(
-                hint: Text("Choose"),
-                focusColor: Colors.amberAccent,
-                items: <String>['Daily', 'Weekly', 'Monthly', 'Annually']
-                    .map((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-                onChanged: (_) {},
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Text(
-                  "Income",
-                  style: TextStyle(
-                    fontFamily: 'JosefinSans',
-                    fontSize: 25.0,
-                    color: Colors.white,
-                  ),
-                ),
-                Text(
-                  "Expenses",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontFamily: 'JosefinSans',
-                    fontSize: 25.0,
-                    color: Colors.white,
-                  ),
-                )
-              ],
-            ),
-            Text(
-              "Total",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontFamily: 'JosefinSans',
-                fontSize: 25.0,
-                color: Colors.white,
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text("Add Transaction"),
-              style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue.shade300,
-                  foregroundColor: Colors.white,
-                  textStyle: TextStyle(fontSize: 20)),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            Container(
-              decoration: BoxDecoration(
-                  color: Colors.white, borderRadius: BorderRadius.circular(20)),
-              padding: const EdgeInsets.all(10),
-              width: 340,
-              height: 260,
-              child: LineChart(
-                LineChartData(
-                  borderData: FlBorderData(show: false),
-                  lineBarsData: [
-                    // The red line
-                    LineChartBarData(
-                      spots: dummyData1,
-                      isCurved: true,
-                      barWidth: 3,
-                      color: Colors.red,
-                    ),
-                    // The orange line
-                    LineChartBarData(
-                      spots: dummyData2,
-                      isCurved: true,
-                      barWidth: 3,
-                      color: Colors.green,
-                    ),
-                  ],
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Text(
+                "Income",
+                style: TextStyle(
+                  fontFamily: 'JosefinSans',
+                  fontSize: 25.0,
+                  color: Colors.white,
                 ),
               ),
+              Text(
+                "Expenses",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontFamily: 'JosefinSans',
+                  fontSize: 25.0,
+                  color: Colors.white,
+                ),
+              )
+            ],
+          ),
+          Text(
+            "Total",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontFamily: 'JosefinSans',
+              fontSize: 25.0,
+              color: Colors.white,
             ),
-          ],
-        )),
-        bottomNavigationBar: BottomNavigationBar(
-          items: const <BottomNavigationBarItem>[
-            BottomNavigationBarItem(
-              icon: Icon(Icons.home),
-              label: 'Home',
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(
-                Icons.query_stats,
+          ),
+          TextButton(
+            // Within the `FirstRoute` widget
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => SecondRoute()),
+              );
+            },
+            child: Text("Add Transaction"),
+            style: TextButton.styleFrom(
+                backgroundColor: Colors.blue.shade300,
+                foregroundColor: Colors.white,
+                textStyle: TextStyle(fontSize: 20)),
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.white, borderRadius: BorderRadius.circular(20)),
+            padding: const EdgeInsets.all(10),
+            width: 340,
+            height: 260,
+            child: LineChart(
+              LineChartData(
+                borderData: FlBorderData(show: false),
+                lineBarsData: [
+                  // The red line
+                  LineChartBarData(
+                    spots: dummyData1,
+                    isCurved: true,
+                    barWidth: 3,
+                    color: Colors.red,
+                  ),
+                  // The orange line
+                  LineChartBarData(
+                    spots: dummyData2,
+                    isCurved: true,
+                    barWidth: 3,
+                    color: Colors.green,
+                  ),
+                ],
               ),
-              label: 'Stats',
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.currency_rupee),
-              label: 'Accounts',
+          ),
+        ],
+      )),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.query_stats,
             ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.more_horiz),
-              label: 'More',
-            ),
-          ],
-          selectedItemColor: Colors.amber[800],
-          unselectedItemColor: Colors.black,
-          showUnselectedLabels: true,
-          currentIndex: _selectedIndex,
-          onTap: _onItemTapped,
-        ),
+            label: 'Stats',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.currency_rupee),
+            label: 'Accounts',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.more_horiz),
+            label: 'More',
+          ),
+        ],
+        selectedItemColor: Colors.amber[800],
+        unselectedItemColor: Colors.black,
+        showUnselectedLabels: true,
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
       ),
+    );
+  }
+}
+
+class SecondRoute extends StatefulWidget {
+  SecondRoute({super.key});
+
+  @override
+  State<SecondRoute> createState() => _SecondRouteState();
+}
+
+class _SecondRouteState extends State<SecondRoute> {
+  final List<bool> _selectedFruits = <bool>[true, false];
+  bool vertical = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Second Route'),
+      ),
+      body: SafeArea(
+          child: Column(
+        children: [
+          Center(
+            child: ToggleButtons(
+              direction: vertical ? Axis.vertical : Axis.horizontal,
+              onPressed: (int index) {
+                setState(() {
+                  // The button that is tapped is set to true, and the others to false.
+                  for (int i = 0; i < _selectedFruits.length; i++) {
+                    _selectedFruits[i] = i == index;
+                  }
+                });
+              },
+              borderRadius: const BorderRadius.all(Radius.circular(8)),
+              selectedBorderColor: Colors.red[700],
+              selectedColor: Colors.white,
+              fillColor: Colors.red[200],
+              color: Colors.red[400],
+              constraints: const BoxConstraints(
+                minHeight: 40.0,
+                minWidth: 80.0,
+              ),
+              isSelected: _selectedFruits,
+              children: fruits,
+            ),
+          )
+        ],
+      )),
     );
   }
 }
